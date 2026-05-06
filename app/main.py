92edbc7592519a633cf55470a1f4089e96005a9e
ManasKohli <manaskohli6@gmail.com>
2026-05-06 16:58:02 -0400

    feat: init o-auth

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from app.routes import auth
 
 app = FastAPI()
 
 
+#auth routes
+app.include_router(auth.router)
+
+
 #start endpoint
+@app.on_event("startup")
+def startup_event():
+    print("Starting up...")
 @app.get("/")
 def read_root():
-    print("running...")
     return {"status": "ok"}
