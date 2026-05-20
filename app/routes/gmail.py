b8d90ca43d44b3a59c908276a53d3652b43d5ae2
ManasKohli <manaskohli6@gmail.com>
2026-05-20 19:05:05 -0400

    feat: config gpt4.1-mini to analyze potential tech emails

--- app/routes/gmail.py
+++ app/routes/gmail.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
-from app.services.gmail_service import get_emails as fetch_emails
+from app.services.gmail_service import fetch_filtered_emails
+from app.services.ai_service import analyze_email_content
 
 router = APIRouter(prefix="/gmail")
 
 @router.get("/emails")
 def get_emails(token: str):
-    results = fetch_emails(token)
+    emails = fetch_filtered_emails(token)
+    results = []
+
+    for email in emails:
+
+        analysis = analyze_email_content(email["subject"], email["snippet"])
+        results.append({'email': email, 'analysis': analysis})
+        
     return {"emails": results}
--- app/services/gmail_service.py
+++ app/services/gmail_service.py
@@ -1,23 +1,44 @@
 import requests
 
-def get_emails(token: str):
-    headers = {"Authorization": f"Bearer {token}"}
+
+def fetch_filtered_emails(token: str):
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
     response = requests.get(
-         "https://gmail.googleapis.com/gmail/v1/users/me/messages",
+        "https://gmail.googleapis.com/gmail/v1/users/me/messages",
         headers=headers,
         params={"maxResults": 10}
     )
 
     email_data = response.json()
+
     messages = email_data.get("messages", [])
 
+    keywords = [
+        "interview",
+        "recruiter",
+        "assessment",
+        "screening",
+        "technical",
+        "coding challenge",
+        "software engineer",
+        "intern"
+    ]
+
     results = []
+
     for msg in messages:
+
         msg_id = msg["id"]
+
         msg_response = requests.get(
             f"https://gmail.googleapis.com/gmail/v1/users/me/messages/{msg_id}",
-              headers=headers
+            headers=headers
         )
+
         data = msg_response.json()
 
         payload = data.get("payload", {})
@@ -27,25 +48,29 @@ def get_emails(token: str):
         sender = ""
 
         for header in headers_list:
+
             if header["name"] == "Subject":
                 subject = header["value"]
+
             elif header["name"] == "From":
                 sender = header["value"]
-        
-        snp = data.get("snippet", "")
-        keywords = [
-        "interview",
-        "recruiter",
-        "assessment",
-        "screening",
-        "technical",
-        "coding challenge",
-        "software engineer",
-        "intern"
-        ]
-        email_text = f"{subject} {snp}".lower()
-        is_potential = any(keyword in email_text for keyword in keywords)
+
+        snippet = data.get("snippet", "")
+
+        email_text = f"{subject} {snippet}".lower()
+
+        is_potential = any(
+            keyword in email_text
+            for keyword in keywords
+        )
+
         if is_potential:
-            results.append({"id": msg_id, "snippet": snp, "subject": subject, "sender": sender})
-            
+
+            results.append({
+                "id": msg_id,
+                "subject": subject,
+                "sender": sender,
+                "snippet": snippet
+            })
+
     return results
